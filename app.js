06de2bcf21fdb35438af6f0531ee8b0a4a2fb6f5
Anand Kumar <anandkumar6204aa@gmail.com>
2026-05-02 23:47:08 +0530

    Migrate MongoDB from local to Atlas and update DB connection logic

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const ejsMate=require("ejs-mate");
 const wrapAsync=require("./utils/wrapAsync.js");
 const Review=require("./models/review.js");
 const{listingSchema,reviewSchema}=require("./schema.js");
-const MONGO_URL="mongodb://127.0.0.1:27017/wonderlust";
+const MONGO_URL = "mongodb+srv://anandkumar6204aa_db_user:anand123@cluster0.7p5mw6i.mongodb.net/wonderlust";
 const ExpressError=require("./utils/ExpressError.js");
 app.use(express.json());
 main()
--- init/index.js
+++ init/index.js
@@ -1,20 +1,25 @@
-const mongoose=require("mongoose");
-const initData=require("./data.js");
-const Listing=require("../models/listing.js");
-const MONGO_URL="mongodb://127.0.0.1:27017/wonderlust";
+const mongoose = require("mongoose");
+const initData = require("./data.js");
+const Listing = require("../models/listing.js");
+
+const MONGO_URL = "mongodb+srv://anandkumar6204aa_db_user:anand123@cluster0.7p5mw6i.mongodb.net/wonderlust";
+
 main()
-.then(()=>{
+.then(async () => {
     console.log("connected to db");
+    await initDB();
+    await mongoose.connection.close();
 })
-.catch((err)=>{
+.catch((err) => {
     console.log(err);
 });
-async function main(){
+
+async function main() {
     await mongoose.connect(MONGO_URL);
 }
- const initDB=async()=>{
+
+const initDB = async () => {
     await Listing.deleteMany({});
     await Listing.insertMany(initData.data);
     console.log("data was initialized");
- };
- initDB();
+};
